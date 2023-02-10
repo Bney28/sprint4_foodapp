19bb045389d463263c840da5cbd19a7da95169b1
Bney28 <bney1@outlook.com>
2023-02-10 08:55:50 -0500

    feat: stylized rest card and filter btns

--- src/Layout/Navbar.jsx
+++ src/Layout/Navbar.jsx
@@ -7,10 +7,10 @@ const Navbar = () => {
         <>
 
             <div className='divNav'>
-                <Link className='divNav__link btn' to="/" ><i className="fa-solid fa-house-chimney divNav__icon"></i></Link>
-                <Link className='divNav__link btn' to="/search" ><i className="fa-solid fa-magnifying-glass divNav__icon"></i></Link>
-                <Link className='divNav__link btn' to="/orders" ><i className="fa-solid fa-clock-rotate-left divNav__icon"></i></Link>
-                <Link className='divNav__link btn' to="/profile" ><i className="fa-regular fa-user divNav__icon"></i></Link>
+                <Link className='divNav__link' to="/" ><i className="fa-solid fa-house-chimney divNav__icon"></i></Link>
+                <Link className='divNav__link' to="/search" ><i className="fa-solid fa-magnifying-glass divNav__icon"></i></Link>
+                <Link className='divNav__link' to="/orders" ><i className="fa-solid fa-clock-rotate-left divNav__icon"></i></Link>
+                <Link className='divNav__link' to="/profile" ><i className="fa-regular fa-user divNav__icon"></i></Link>
             </div>
 
             <div>
