d4cb7fdba096a05fecf53adb44a876a2c2a45c26
Bney28 <bney1@outlook.com>
2023-02-12 16:27:44 -0500

    feat: added firebase

---
+++ src/api/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAC6Gh3UqwPwg3lTIGf-DTT4Ppr2ctZzgI",
+  authDomain: "food-app-s4-20245.firebaseapp.com",
+  projectId: "food-app-s4-20245",
+  storageBucket: "food-app-s4-20245.appspot.com",
+  messagingSenderId: "8509579154",
+  appId: "1:8509579154:web:3284dffaec25c3f2a22b4b"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
