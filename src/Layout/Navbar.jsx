72d5475c62c88200a527dd86adf6d65379e52a61
Bney28 <bney1@outlook.com>
2023-02-14 14:28:10 -0500

    feat: new styles added

--- src/Layout/Navbar.jsx
+++ src/Layout/Navbar.jsx
@@ -5,6 +5,22 @@ import '../Styles/Navbar.sass'
 const Navbar = () => {
     return (
         <>
+            <div className="icons-cel">
+                <div>
+                    <p className='icons-cel__hour'>11:11</p>
+                </div>
+
+                <div className='social-media'>
+                <i className="fa-brands fa-instagram social-media__icon"></i>
+                    <i className="fa-brands fa-tiktok social-media__icon"></i>
+                </div>
+
+                <div className='div-icons'>
+                    <i className="fa-solid fa-signal div-icons__icon"></i>
+                    <i className="fa-solid fa-wifi div-icons__icon"></i>
+                    <i className="fa-solid fa-battery-half div-icons__icon"></i>
+                </div>
+            </div>
 
             <div className='divNav'>
                 <Link className='divNav__link' to="/" ><i className="fa-solid fa-house-chimney divNav__icon"></i></Link>
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,33 +1,37 @@
 import React, { useContext } from 'react'
 import img from '../Imgs/Img.png'
+import desc from '../Imgs/4600925.jpg'
 import '../Styles/Home.sass'
 import { Navigate } from 'react-router-dom'
-import { auth} from '../Context'
+import { auth } from '../Context'
 
 
 const Home = () => {
-  
+
   const { user } = useContext(auth.authContext)
 
   return (
     <>
       {user ?
         <>
-          <div>Home</div>
-
           <div className="cont-ppal">
             <div className="card-desc">
-              Descuentos en Carrusel ._.
+              <img src="https://res.cloudinary.com/dxre2n1ja/image/upload/v1676398916/food-app/Promo_1_jvxz2w.png" alt="image-descount" className='card-desc__imgn'/>
+              <img src="https://res.cloudinary.com/dxre2n1ja/image/upload/v1676399462/food-app/Promo_2_hgtikp.png" alt="image-descount" className='card-desc__imgn'/>
+              <img src="https://res.cloudinary.com/dxre2n1ja/image/upload/v1676177775/food-app/4600925_o7wkl7.jpg" alt="image-descount" className='card-desc__imgn'/>
             </div>
 
             <p>Restaurants and Cafes</p>
 
             <div className="btn-filter">
               <button className="btn">All</button>
-              <button className="btn"><i className="fa-solid fa-burger btn-filter__icon"></i>Fast Food</button>
-              <button className="btn"><i className="fa-solid fa-pizza-slice btn-filter__icon"></i>Pizzas</button>
-              <button className="btn"><i className="fa-solid fa-bowl-food btn-filter__icon"></i>Salads</button>
-              <button className="btn"><i className="fa-solid fa-champagne-glasses btn-filter__icon"></i>Drinks</button>
+              <button className="btn"><i className="fa-solid fa-burger btn-filter__icon"></i>Burger</button>
+              <button className="btn"><i className="fa-solid fa-pizza-slice btn-filter__icon"></i>Pizza</button>
+              <button className="btn"><i className="fa-solid fa-bowl-food btn-filter__icon"></i>Salad</button>
+              <button className="btn"><i className="fa-solid fa-bacon btn-filter__icon"></i>Pasta</button>
+              <button className="btn"><i className="fa-solid fa-bowl-rice btn-filter__icon"></i>Fries</button>
+              <button className="btn"><i className="fa-solid fa-drumstick-bite btn-filter__icon"></i>Wings</button>
+              <button className="btn"><i className="fa-solid fa-utensils btn-filter__icon"></i>Soup</button>
             </div>
 
 
@@ -41,13 +45,9 @@ const Home = () => {
                 <h5 className="card-rest__title">Pardes Restaurant</h5>
                 <p className="card-rest__stars">
                   <i className="fa-solid fa-star card-rest__icon"></i>
-                  <i className="fa-solid fa-star card-rest__icon"></i>
-                  <i className="fa-solid fa-star card-rest__icon"></i>
-                  <i className="fa-solid fa-star card-rest__icon"></i>
-                  <i className="fa-solid fa-star card-rest__icon"></i>
                 </p>
                 <p className="card-rest__text">Work time 09:30-23:00</p>
-                <p className="card-rest__price">Before you 4$</p>
+                <p className="card-rest__price">Cost 4$</p>
               </div>
 
             </div>
