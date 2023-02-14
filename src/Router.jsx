181793ac00b395dc5e76b3ea3e8218a82830f7d8
Bney28 <bney1@outlook.com>
2023-02-13 20:34:33 -0500

    feat: added Login component to Router

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,18 +1,18 @@
 import React from 'react'
-import { Home, Orders, Product, Profile, Search } from './Pages'
+import { Home, Orders, Product, Profile, Search, Login } from './Pages'
 import { Navbar } from './Layout'
 import { createBrowserRouter, createRoutesFromElements, Route } from 'react-router-dom'
 
 export const router = createBrowserRouter(
     createRoutesFromElements(
         <>
+            <Route path="/login" element={<Login />} />
             <Route path="/" element={<Navbar />} >
                 <Route path="/" element={<Home />}/>
                 <Route path="/product" element={<Product />}/>
                 <Route path="/search" element={<Search />}/>
                 <Route path="/orders" element={<Orders />}/>
                 <Route path="/profile" element={<Profile />}/>
-
             </ Route>
 
         </>
