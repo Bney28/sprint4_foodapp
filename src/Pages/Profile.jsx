9daac493ff7a7ad7d75b713e929f1b3f50ba6a38
Bney28 <bney1@outlook.com>
2023-02-15 00:08:14 -0500

    feat: added styles to Profile

--- src/Pages/Profile.jsx
+++ src/Pages/Profile.jsx
@@ -1,8 +1,47 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { Navigate } from 'react-router-dom'
+import { auth as authApi } from '../api'
+import { auth as authCont } from '../Context'
+import logo from '../Imgs/Logo.png'
+import '../Styles/Profile.sass'
 
 const Profile = () => {
+
+  const { user } = useContext(authCont.authContext)
+  console.log(user);
+
+  const { displayName, email, photoURL, phoneNumber, birdthDate } = user
+
+  const handleLogout = () => {
+    authApi.logout()
+  }
+
   return (
-    <div>Profile</div>
+    <>
+      {user ?
+        <>
+          <div className="cont-ppal-profile">            
+            <div className="image-profile">
+              <img src={logo} alt="profile" />
+            </div>
+            <div className="info-profile">
+              <h2>¡Bienvenido/a!</h2>
+              <h6><input type="text" placeholder='Name' value={displayName}/></h6>
+              <h6><input type="text" placeholder='E-mail' value={email}/></h6>
+              <h6><input type="text" placeholder='Phone Number' value={phoneNumber}/></h6>
+              <h6><input type="text" placeholder='Birth Date' value={birdthDate}/></h6>
+            </div>
+            <div className='btn-logout'>
+              <button onClick={handleLogout} className="btn">Log Out</button>
+            </div>
+          </div>
+        </>
+        :
+        <Navigate to="/login" />
+
+      }
+
+    </>
   )
 }
 
