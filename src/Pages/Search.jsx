ce9f65b2aef22a95dc00a326289687f8454dde5a
Bney28 <bney1@outlook.com>
2023-02-16 15:14:33 -0500

    feat: added styles to search

--- src/Pages/Search.jsx
+++ src/Pages/Search.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
+import '../Styles/General.sass'
 
 const Search = () => {
+
+  // Funcionalidades en este Componente
+  // - Filtro por el input para los platos
+
   return (
-    <div>Search</div>
+    <>
+      <div className='divSearch'>
+        <div className="divSearch__iconInput">
+          <i className="fa-solid fa-magnifying-glass"></i>
+          <input type="text" placeholder='Search for a dish' />
+        </div>
+        <div className="divSearch__text">
+          <p>Recent Searches</p>
+        </div>
+      </div>
+    </>
   )
 }
 
